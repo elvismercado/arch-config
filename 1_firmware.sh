7ae2b5f9830b1c515c74d9d8d6a30d367d968041
JuveScript <github@juvescript.com>
2024-05-24 11:24:16 +0200

    Update

--- 1_firmware.sh
+++ 1_firmware.sh
@@ -3,6 +3,6 @@
 source setup_script_exit.sh
 setup_script_exit
 
-yay -S --noconfirm --needed --disable-download-timeout mkinitcpio-firmware # Optional firmware for the default linux kernel to get rid of the annoying 'WARNING: Possibly missing firmware for module:' messages
+yay -S --noconfirm --needed --disable-download-timeout --removemake mkinitcpio-firmware # Optional firmware for the default linux kernel to get rid of the annoying 'WARNING: Possibly missing firmware for module:' messages
 
 sudo mkinitcpio -P
