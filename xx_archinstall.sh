b7beb1774771dd6f5e7d60d9777785305583c29a
JuveScript <github@juvescript.com>
2024-06-27 10:41:04 +0200

    Update

--- xx_archinstall.sh
+++ xx_archinstall.sh
@@ -4,8 +4,6 @@
 
 # `lsblk`: find usb device
 
-# 
-
 # mount --mkdir /dev/[lsblk device name] /mnt/[lsblk device name]
 
 # archinstall --config /mnt/[lsblk device name]/user_configuration.json --creds /mnt/[lsblk device name]/user_credentials.json 
