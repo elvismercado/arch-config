ad01b4dedc914cf5d86766133a5914e85b1c41da
JuveScript <github@juvescript.com>
2024-06-27 12:07:46 +0200

    Update

--- 30_after_login.sh
+++ 30_after_login.sh
@@ -3,6 +3,13 @@
 source setup_script_exit.sh
 setup_script_exit
 
+# ACPI
+systemctl enable --now acpid
+
+# Printing
+systemctl enable --now avahi-daemon
+systemctl enable --now cups
+
 # First make sure to edit your monitor configuration
 # Logout and relogin afterwards
 sudo cp ~/.config/monitors.xml ~gdm/.config/
